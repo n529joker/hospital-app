69b56b0c28ecef325bccfcd4b6c8d5f33d8eb621
ngenge <ngengerawlings20@gmail.com>
2022-09-06 13:39:59 +0100

    data

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const cors = require('cors')
 const axios = require('axios')
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const app = express()
-const port = process.env.PORT || 3000
+const port = 3000
 
 app.use(cors({
-  origin:'http://127.0.0.1:5500'
-}))
+  origin:'http://localhost:5500'
+}
+)) 
 app.use(express.json())
 //app.use(express.static('public'))
 
@@ -16,12 +17,36 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 let collection;
 
-async function fetchData(data){
+ 
+app.get('/',async(req, res)=>{
+  let data = await axios('https://api.geoapify.com/v1/ipinfo?&apiKey=2fe3fe325ed9460fa19f7cfc7b771676')
+  let fdata = await data.data
+  //console.log(fdata);
   try{
     await client.connect(err => {
     console.log("MongoDB connected")
     const ids = client.db("forApp").collection("hospitals")
-    ids.find({'reg':{'$eq':data}}).toArray((err, result)=> {
+    ids.find({'reg':{'$eq':fdata.state.name}}).toArray((err, result)=> {
+      console.log(result);
+       res.json(result)
+    })
+});
+  }catch(ex){
+    console.error(ex)
+  }
+  finally{
+    await client.close()
+  }
+ // res.send('hi')
+})
+
+app.post('/data',async (req,res)=>{
+  let region = req.body.reg
+  try{
+    await client.connect(err => {
+    console.log("MongoDB connected")
+    const ids = client.db("forApp").collection("hospitals")
+    ids.find({'reg':{'$eq':region}}).toArray((err, result)=> {
       console.log(result);
        res.jsonp(result)
     })
@@ -32,16 +57,7 @@ async function fetchData(data){
   finally{
     await client.close()
   }
-}
-
-app.listen(port) 
-app.get('/',async(req, res)=>{
-  let data = await axios('https://api.geoapify.com/v1/ipinfo?&apiKey=2fe3fe325ed9460fa19f7cfc7b771676')
-  let fdata = await data.data
-  fetchData(fdata.state.name)
 })
-
-app.post('/data',async (req,res)=>{
-  let region = req.body.reg
-  fetchData(region)
+app.listen(port,()=>{
+  console.log(`app running on port ${port}`);
 })
