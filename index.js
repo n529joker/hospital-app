84a863c7c635e01150b7969a195899f5f734c63b
ngenge <ngengerawlings20@gmail.com>
2022-09-06 13:45:00 +0100

    Errors

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const axios = require('axios')
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(cors({
   origin:'http://localhost:5500'
