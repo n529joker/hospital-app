2dd4799637748fd063435bc2ad9d35da9fbaece2
ngenge <ngengerawlings20@gmail.com>
2022-09-06 10:50:45 +0100

    All is done

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 const express = require('express')
 const cors = require('cors')
+const axios = require('axios')
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('public'))
-
-app.listen(port) 
+//app.use(express.static('public'))
 
 const uri = "mongodb+srv://infinitycloud:Rawlings004@infinitycloud.0vlqb7r.mongodb.net?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 let collection;
 
-app.post('/data',async (req,res)=>{
-  let region = req.body.reg
+async function fetchData(data){
   try{
     await client.connect(err => {
     console.log("MongoDB connected")
     const ids = client.db("forApp").collection("hospitals")
-    ids.find({'reg':{'$eq':region}}).toArray(function(err, result) {
-       res.jsonp(result)
+    ids.find({'reg':{'$eq':data}}).toArray((err, result)=> {
+      console.log(result);
+       res.send('result')
     })
 });
   }catch(ex){
@@ -31,5 +30,16 @@ app.post('/data',async (req,res)=>{
   finally{
     await client.close()
   }
+}
+
+app.listen(port, console.log('Live')) 
+app.get('/',async(req, res)=>{
+  let data = await axios('https://api.geoapify.com/v1/ipinfo?&apiKey=2fe3fe325ed9460fa19f7cfc7b771676')
+  let fdata = await data.data
+  fetchData(fdata.state.name)
 })
 
+app.post('/data',async (req,res)=>{
+  let region = req.body.reg
+  fetchData(region)
+})
