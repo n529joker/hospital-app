9a4e45b65dda1c635d6f2112f4b4dd5640c46d29
Rawlings Ngenge <69519770+n529joker@users.noreply.github.com>
2022-09-08 12:51:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,30 +9,14 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
 
+var collection;
 const uri = "mongodb+srv://infinitycloud:Rawlings004@infinitycloud.0vlqb7r.mongodb.net?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-let collection;
-app.post('/data',async (req,res)=>{
-  let Region = req.body.region.reg
-  try{
-    await client.connect(err => {
-    const ids = client.db("forApp").collection("hospitals")
-    ids.find({'reg':{'$eq':Region}}).toArray((err, result)=> {
-      console.log(result)
-       res.jsonp(result)
-    })
-});
-  }catch(ex){
-    console.error(ex)
-  }
-  finally{
-    await client.close()
-  }
-})
-app.get('/',(req,res)=>{
-res.send('HW')
-})
+app.listen(port,()=>{console.log(`app running on port ${port}`)}) 
+
+app.get('/',()=>{console.log('connected')})
+
 app.get('/fdata',async (req,res)=>{
   try{
     await client.connect(err => {
@@ -48,6 +32,24 @@ app.get('/fdata',async (req,res)=>{
     await client.close()
   }
 })
-app.listen(port,()=>{
-  console.log(`app running on port ${port}`);
-}) 
+
+app.post('/data',async (req,res)=>{
+  let Region = req.body.reg.region
+  try{
+    await client.connect(err => {
+    const ids = client.db("forApp").collection("hospitals")
+    ids.find({'reg':{'$eq':Region}}).toArray((err, result)=> {
+      console.log(result)
+       res.jsonp(result)
+    })
+});
+  }catch(ex){
+    console.error(ex)
+  }
+  finally{
+    await client.close()
+  }
+})
+
+
+
