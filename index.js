1efb1c4beb9410cb43bd95e4755b2927ae8b8ab7
Rawlings Ngenge <69519770+n529joker@users.noreply.github.com>
2022-11-07 07:58:27 +0100

    changed route to '/'

--- index.js
+++ index.js
@@ -13,11 +13,9 @@ var collection;
 const uri = "mongodb+srv://infinitycloud:Rawlings004@infinitycloud.0vlqb7r.mongodb.net?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-app.listen(port,()=>{console.log(`app running on port ${port}`)}) 
 
-app.get('/',()=>{console.log('connected')})
 
-app.get('/fdata',async (req,res)=>{
+app.get('/',async (req,res)=>{
   try{
     await client.connect(err => {
     const ids = client.db("forApp").collection("hospitals")
@@ -33,23 +31,6 @@ app.get('/fdata',async (req,res)=>{
   }
 })
 
-// app.post('/data',async (req,res)=>{
-//   let Region = req.body.reg.region
-//   try{
-//     await client.connect(err => {
-//     const ids = client.db("forApp").collection("hospitals")
-//     ids.find({'reg':{'$eq':Region}}).toArray((err, result)=> {
-//       console.log(result)
-//        res.jsonp(result)
-//     })
-// });
-//   }catch(ex){
-//     console.error(ex)
-//   }
-//   finally{
-//     await client.close()
-//   }
-// })
-
+app.listen(port,()=>{console.log(`app running on port ${port}`)}) 
 
 
